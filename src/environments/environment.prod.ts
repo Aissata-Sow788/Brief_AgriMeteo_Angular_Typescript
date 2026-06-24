da1c3e40cbea69c95b1ef7b3b0a2ec6c5dc584a7
Aissata-Sow788 <saissata676@gmail.com>
2026-06-24 18:24:32 +0000

    version finale

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  openWeatherApiKey: '',
+  openWeatherApiKey: '302995af23ca8495196cf8ed3c1ebae3',
   openWeatherBaseUrl: 'https://api.openweathermap.org/data/2.5'
 };
